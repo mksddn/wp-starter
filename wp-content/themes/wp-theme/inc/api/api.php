756f97413261b1183356cb237ff8653c18d6b4b7
mksddn <maksim.anatolyevitch@gmail.com>
2024-06-15 08:48:11 +0300

    Enable CORS on JSON API Wordpress

--- wp-content/themes/wp-theme/inc/api/api.php
+++ wp-content/themes/wp-theme/inc/api/api.php
@@ -1,4 +1,11 @@
 <?php
 
+// Enable CORS on JSON API Wordpress
+function add_cors_http_header()
+{
+  header("Access-Control-Allow-Origin: *");
+}
+add_action('init', 'add_cors_http_header');
+
 require 'PageController.php';
 require 'forms/index.php';
