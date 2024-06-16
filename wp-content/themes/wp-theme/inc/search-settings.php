a4a66c2a071827ee2cc4eefcbf567f608711eebc
mksddn <maksim.anatolyevitch@gmail.com>
2024-06-16 15:09:08 +0300

    searchfilter

--- wp-content/themes/wp-theme/inc/search-settings.php
+++ wp-content/themes/wp-theme/inc/search-settings.php
@@ -3,14 +3,14 @@
 /**
  * Показываем только определенные типы записей в результатах поиска
  */
-function searchfilter($query)
-{
-  if ($query->is_search && !is_admin()) {
-    $query->set('post_type', array('post', 'page', 'product'));
-  }
-  return $query;
-}
-add_filter('pre_get_posts', 'searchfilter');
+// function searchfilter($query)
+// {
+//   if ($query->is_search && !is_admin()) {
+//     $query->set('post_type', array('post', 'page', 'product'));
+//   }
+//   return $query;
+// }
+// add_filter('pre_get_posts', 'searchfilter');
 
 
 /**
