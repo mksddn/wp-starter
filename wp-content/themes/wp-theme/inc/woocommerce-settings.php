33532fd5686e1e5686984f62828566ef4b3c3815
mksddn <maksim.anatolyevitch@gmail.com>
2024-12-11 13:17:07 +0300

    add wc checking

--- wp-content/themes/wp-theme/inc/woocommerce-settings.php
+++ wp-content/themes/wp-theme/inc/woocommerce-settings.php
@@ -1,6 +1,8 @@
 <?php
-add_action('after_setup_theme', 'woocommerce_support');
-function woocommerce_support()
-{
-  add_theme_support('woocommerce');
+if (class_exists('WooCommerce')) {
+  add_action('after_setup_theme', 'woocommerce_support');
+  function woocommerce_support()
+  {
+    add_theme_support('woocommerce');
+  }
 }
