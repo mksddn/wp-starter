d4d64ef8cb9b8653774f5d5bbba17ce0ece9b846
mksddn <maksim.anatolyevitch@gmail.com>
2024-12-15 14:07:15 +0300

    remove extra api code

--- wp-content/themes/wp-theme/inc/api/api.php
+++ wp-content/themes/wp-theme/inc/api/api.php
@@ -1,12 +1,11 @@
 <?php
 
-// Enable CORS on JSON API Wordpress
-function add_cors_http_header()
-{
-  header("Access-Control-Allow-Origin: *");
-}
-add_action('init', 'add_cors_http_header');
+// // Enable CORS on JSON API Wordpress
+// function add_cors_http_header()
+// {
+//   header("Access-Control-Allow-Origin: *");
+// }
+// add_action('init', 'add_cors_http_header');
 
-require 'PageController.php';
 require 'forms/index.php';
 
